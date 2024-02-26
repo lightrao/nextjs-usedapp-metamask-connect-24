89a8f101444a4db931997c4d995dd1cf55abb1c0
lightrao <lightrao@hotmail.com>
2024-02-26 08:04:26 +0800

    done

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
 import "@/styles/globals.css";
+import { DAppProvider } from "@usedapp/core";
+
+import { contractAddress } from "../constants/contract-address";
+
+const config = {
+  multicallAddresses: [contractAddress],
+};
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <DAppProvider config={config}>
+      <Component {...pageProps} />
+    </DAppProvider>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,40 @@
 import Head from "next/head";
-import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+
+import { useState, useEffect } from "react";
+
+import { ethers } from "ethers";
+import { useEthers, useContractFunction } from "@usedapp/core";
+
+import { abi } from "../constants/abi";
+import { contractAddress } from "../constants/contract-address";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const { activateBrowserWallet, account } = useEthers();
+  const [hasMetamask, setHasMetamask] = useState(false);
+
+  useEffect(() => {
+    if (typeof window.ethereum !== "undefined") {
+      setHasMetamask(true);
+    }
+  });
+
+  async function connect() {
+    await activateBrowserWallet();
+  }
+
+  const contract = new ethers.Contract(contractAddress, abi);
+  const { send, state } = useContractFunction(contract, "store", {
+    transactionName: "store",
+  });
+
+  useEffect(() => {
+    console.log(`State: ${state.status}`);
+  }, [state]);
+
   return (
     <>
       <Head>
@@ -15,98 +44,18 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.js</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{" "}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
+        <div>
+          {" "}
+          {hasMetamask ? (
+            account ? (
+              "Connected! "
+            ) : (
+              <button onClick={() => connect()}>Connect</button>
+            )
+          ) : (
+            "Please install metamask"
+          )}
+          {account ? <button onClick={() => send(42)}>Execute</button> : ""}
         </div>
       </main>
     </>
